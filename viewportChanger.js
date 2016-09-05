5a3512f7e3b25a08b2b619e9ebe756efb3ccd6f9
Vadim Zhukov <zhukov@easykod.ru>
2016-09-05 13:29:32 +0300

    windowResized

---
+++ viewportChanger.js
@@ -0,0 +1,31 @@
+window.addEventListener('resize', windowResized);
+
+function windowResized(argument) {
+	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+		var mobile = 320;
+		var pad = 768;
+
+		var deviceWidth = (document.body.clientWidth < window.screen.width) ? document.body.clientWidth : window.screen.width;
+		var deviceHeight = (document.body.clientHeight < window.screen.height) ? document.body.clientHeight : window.screen.height;
+
+		var orientation = (deviceWidth > deviceHeight) ? 'landscape' : 'portrait';
+		var dpt = window.devicePixelRatio = window.devicePixelRatio || Math.round(window.screen.availWidth / document.documentElement.clientWidth);
+		if (dpt > 2) dpt = 1.5;
+
+		var widthDPI = window.screen.width * dpt;
+		var heightDPI = window.screen.height * dpt;
+
+		if (orientation == 'landscape') {
+			document.getElementById('viewport').setAttribute('content', 'width=' + pad + ',  user-scalable=no');
+		} else {
+			if (widthDPI >= pad) {
+				document.getElementById('viewport').setAttribute('content', 'width=' + pad + ',  user-scalable=no');
+			} else {
+				document.getElementById('viewport').setAttribute('content', 'width=' + mobile);
+			}
+		}
+
+	}
+}
+
+window.dispatchEvent( new Event('resize') );
